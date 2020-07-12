cd3414319215e1fa86f28199a5f34c2962370e7e
Meet <meetgandhi14@gmail.com>
2020-07-12 16:00:49 +0530

    Update AudioRequestor.cs

--- UnityProject/Assets/NetMQExample/Scripts/AudioRequestor.cs
+++ UnityProject/Assets/NetMQExample/Scripts/AudioRequestor.cs
@@ -6,17 +6,8 @@ using System;
 using System.Text;
 using WavUtils;
 
-/// <summary>
-///     Example of requester who only sends Hello. Very nice guy.
-///     You can copy this class and modify Run() to suits your needs.
-///     To use this class, you just instantiate, call Start() when you want to start and Stop() when you want to stop.
-/// </summary>
 public class AudioRequester : RunAbleThread
 {       
-    /// <summary>
-    ///     Request Hello message to server and receive message back. Do it 10 times.
-    ///     Stop requesting when Running=false.
-    /// </summary>
 protected override void Run()
     {  
         ForceDotNet.Force(); // this line is needed to prevent unity freeze after one use, not sure why yet
@@ -28,11 +19,7 @@ protected override void Run()
             {
                 Debug.Log("Sending: Hello");
                 client.SendFrame("Hello");
-                // ReceiveFrameString() blocks the thread until you receive the string, but TryReceiveFrameString()
-                // do not block the thread, you can try commenting one and see what the other does, try to reason why
-                // unity freezes when you use ReceiveFrameString() and play and stop the scene without running the server
-//                string message = client.ReceiveFrameString();
-//                Debug.Log("Received: " + message);
+
                 bool gotMessage = false;
                 byte[] messagebytes = null; 
                 while (Running)
@@ -40,10 +27,6 @@ protected override void Run()
                     gotMessage = client.TryReceiveFrameBytes(out messagebytes); // this returns true if it's successful
                     if (gotMessage) 
                     {
-                        //messagebytes = Encoding.ASCII.GetBytes(message); 
-                        //System.IO.File.WriteAllBytes("yourfilepath.wav", messagebytes);
-                        //Debug.Log(messagebytes.Length);
-
                         Dispatcher.ExecuteOnMainThread.Enqueue(() =>
                         {
                         WAV wav = new WAV(messagebytes);
